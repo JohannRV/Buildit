fbee2fba4986f3af15907302a36a4ac59396ca09
JohannRV <johnrv.developer@gmail.com>
2023-05-29 11:13:59 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Router } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
+import { Navbar } from "./components";
 
 const app = () => {
   return (
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/about" exact element={<About />} />
-      <Route path="/projects" exact element={<Projects />} />
-      <Route path="/contact" exact element={<Contact />} />
-      <Route path="/services" exact element={<Services />} />
-    </Routes>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/projects" exact element={<Projects />} />
+        <Route path="/contact" exact element={<Contact />} />
+        <Route path="/services" exact element={<Services />} />
+      </Routes>
+    </Router>
   );
 };
 
